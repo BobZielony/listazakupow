9e5622c6789da805e85de4ea14ef718496307445
BobZielony <57916999+BobZielony@users.noreply.github.com>
2022-02-07 08:07:44 +0100

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import List from "./components/List.js";
+import "./components/List.css";
+
+
+  
+ReactDOM.render(
+    <div>
+        <List/>
+    </div>,
+    document.getElementById("container")
+);
